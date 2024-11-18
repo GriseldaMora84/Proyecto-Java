2ae3b5642a2c78a23087d156eb601bac779d453c
Lizra <Lizra@LizRF>
2024-11-18 15:41:15 -0800

    111111

--- src/interfaz/Menu.java
+++ src/interfaz/Menu.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class Menu extends JDialog {
 
@@ -32,9 +33,13 @@ public class Menu extends JDialog {
 	public Menu() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ESTE VA A SER EL MENÚ PRINCIPAL");
+		lblNewLabel.setBounds(54, 54, 342, 58);
+		contentPanel.add(lblNewLabel);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -52,5 +57,4 @@ public class Menu extends JDialog {
 			}
 		}
 	}
-
 }
