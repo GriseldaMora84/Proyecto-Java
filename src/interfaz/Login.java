1fb71a4822eedb45e16962db9e1edfcc4f8b9f8e
Lizra <Lizra@LizRF>
2024-11-21 14:26:37 -0800

    ggggg

--- src/interfaz/Login.java
+++ src/interfaz/Login.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -58,8 +60,10 @@ public class Login extends JDialog {
 	}
 
 	public Login() {
+		
 		setTitle("Login");
-
+		Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("src/pictures/Car-icon"));
+        setIconImage(icon); 
 		setForeground(new Color(0, 0, 0));
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
